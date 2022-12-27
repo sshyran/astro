43777b7f1a68e446494c9f88def678f2913d0065
bholmesdev <hey@bholmes.dev>
2022-12-27 12:46:34 -0500

    feat: change extendDefaults -> gfm

--- packages/markdown/remark/src/types.ts
+++ packages/markdown/remark/src/types.ts
@@ -43,7 +43,7 @@ export interface AstroMarkdownOptions {
 	remarkPlugins?: RemarkPlugins;
 	rehypePlugins?: RehypePlugins;
 	remarkRehype?: RemarkRehype;
-	extendDefaultPlugins?: boolean;
+	githubFlavoredMarkdown?: boolean;
 }
 
 export interface MarkdownRenderingOptions extends AstroMarkdownOptions {
