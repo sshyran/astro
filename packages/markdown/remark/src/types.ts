ffc64cb8c34b5bdf948dbcb44438c5192572fcd3
bholmesdev <hey@bholmes.dev>
2022-12-27 15:20:06 -0500

    chore: remove unused `mode` property

--- packages/markdown/remark/src/types.ts
+++ packages/markdown/remark/src/types.ts
@@ -36,7 +36,6 @@ export interface ShikiConfig {
 }
 
 export interface AstroMarkdownOptions {
-	mode?: 'md' | 'mdx';
 	drafts?: boolean;
 	syntaxHighlight?: 'shiki' | 'prism' | false;
 	shikiConfig?: ShikiConfig;
